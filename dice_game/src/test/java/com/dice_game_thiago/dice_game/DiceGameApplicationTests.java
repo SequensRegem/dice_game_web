e97831f18dee62c028a785ffc5819a98e12f173d
Pedro <pedrooliveira7031@gmail.com>
2025-10-29 08:57:31 -0300

    adding the project itself

---
+++ dice_game/src/test/java/com/dice_game_thiago/dice_game/DiceGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dice_game_thiago.dice_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiceGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
